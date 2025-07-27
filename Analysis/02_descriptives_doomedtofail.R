927d4f71cc2f75782dc91e345c73139788bc926f
Simon <da.simon@dipf.de>
2025-07-27 12:13:31 +0200

    typo

--- Analysis/02_descriptives_doomedtofail.R
+++ Analysis/02_descriptives_doomedtofail.R
@@ -15,6 +15,7 @@
 ####  ------------------------- (1) Load Packages -------------------------  ####
 library(tidyverse)
 library(corrtable)
+library(finalfit) 
 
 
 ####  ----------------------- (2) Read MPlus Results ----------------------- ####
@@ -22,10 +23,11 @@ library(corrtable)
 # MPlus import
 nclass <- 3
 data_lca <- read.table(paste0("Data_Gen/LCA_Results/lca_cprob_3class.dat"), na.strings = "9999.000", sep = "", header = FALSE,
-                       col.names = c("big_ope", "big_con", "big_ext", "big_agr", "big_neu",
+                       col.names = c("par_edu",
+                                     "big_ope", "big_con", "big_ext", "big_agr", "big_neu",
                                      "int_edi", "int_ssi", "int_abi", "ext_uti", "ext_lod", "ext_soi",
-                                     "aca_abi", 
-                                     "par_edu", "hisei",
+                                     "aca_abi",
+                                      "hisei",
                                      "str_aca_int", "nor_aca_int", "pee_soc_int", "fac_soc_int", 
                                      "dro_int", "dro_out",
                                      paste0("cprob", 1:nclass),
@@ -71,31 +73,90 @@ data_cor <- data %>%
   dplyr::select(big_ope, big_con, big_ext, big_agr, big_neu,
                 int_edi, int_ssi, int_abi, ext_uti, ext_lod, ext_soi,
                 aca_abi, 
-                par_edu1, par_edu2, par_edu3, hisei, 
-                str_aca_int, nor_aca_int, pee_soc_int, fac_soc_int, 
-                dro_out)
+                par_edu, par_edu1, par_edu2, par_edu3, hisei,
+                dro_out,
+                str_aca_int, nor_aca_int, pee_soc_int, fac_soc_int)
 
 cor_tab <- corrtable::correlation_matrix(
   data_cor,
   type = "spearman",
   digits = 2,
   decimal.mark = ".",
-  use = "upper",
+  use = "all",
   show_significance = TRUE,
   replace_diagonal = FALSE,
   replacement = ""
 )
 
-cor_tab <- cor_tab[,17:21]
+cor_tab <- cor_tab[,18:22]
+
+# Descriptive statistics
+data_des <- data_cor %>%
+  dplyr::mutate(
+    par_edu = as.factor(par_edu),
+    dro_out = as.factor(dro_out))
+
+finalfit::ff_glimpse(data_des, digits = 2)
 
 
 ####  ----------------------------- (5) ANOVA ----------------------------- ####
+data_anova <- data %>%
+  dplyr::mutate(
+    par_edu1 = if_else(par_edu == 1, 1, 0),
+    par_edu2 = if_else(par_edu == 2, 1, 0),
+    par_edu3 = if_else(par_edu == 3, 1, 0),
+    sub1 = if_else(tg24202_g1 == 1, 1, 0),
+    sub2 = if_else(tg24202_g1 == 2, 1, 0),
+    sub3 = if_else(tg24202_g1 == 3, 1, 0),
+    sub4 = if_else(tg24202_g1 == 4, 1, 0),
+    sub5 = if_else(tg24202_g1 == 5, 1, 0),
+    sub6 = if_else(tg24202_g1 == 6, 1, 0)) %>%
+  dplyr::select(big_ope, big_con, big_ext, big_agr, big_neu,
+                int_edi, int_ssi, int_abi, ext_uti, ext_lod, ext_soi,
+                aca_abi, 
+                par_edu1, par_edu2, par_edu3, hisei,
+                dro_out,
+                str_aca_int, nor_aca_int, pee_soc_int, fac_soc_int,
+                sub1, sub2, sub3, sub4, sub5, sub6, 
+                age, gender,
+                class)%>%
+  dplyr::mutate(
+    par_edu1 = as.factor(par_edu1),
+    par_edu2 = as.factor(par_edu2),
+    par_edu3 = as.factor(par_edu3),
+    dro_out = as.factor(dro_out),
+    gender = as.factor(gender),
+    sub1 = as.factor(sub1),
+    sub2 = as.factor(sub2),
+    sub3 = as.factor(sub3),
+    sub4 = as.factor(sub4),
+    sub5 = as.factor(sub5),
+    sub6 = as.factor(sub6),
+    class = as.factor(class))
 
+# Descriptive statistics per class
+
+#class 1
+data_des_class1 <- data_anova %>%
+  dplyr::filter(class == 1)
+
+finalfit::ff_glimpse(data_des_class1, digits = 2)
+
+#class 2
+data_des_class2 <- data_anova %>%
+  dplyr::filter(class == 2)
+
+finalfit::ff_glimpse(data_des_class2, digits = 2)
+
+#class 3
+data_des_class3 <- data_anova %>%
+  dplyr::filter(class == 3)
+
+finalfit::ff_glimpse(data_des_class3, digits = 2)
 
 # check differences
 
 Lerhamt
-Fächer (Stem, ja/ nein) tg24170_g5
 Age 
 Gender
 
