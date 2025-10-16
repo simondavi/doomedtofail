b4dc4cae1305eb0d363aa0d85421cbb03b267ce9
Simon <da.simon@dipf.de>
2025-10-16 13:49:05 +0200

    padjust

---
+++ Analysis/06_padjustintegration_doomedtofail.R
@@ -0,0 +1,26 @@
+# 06_padjustintegration_doomedtofail.R
+#
+# Comment: 
+#
+# Input: mi_model2.out
+# Output: 
+#
+# Contents: (1) Load Packages
+#           (2) Read MPlus Output
+#           (3) Adjust P-values
+
+####  ------------------------- (1) Load Packages ------------------------  ####
+library(dplyr)
+library(MplusAutomation)
+
+
+####  ----------------------- (2) Read MPlus Output ----------------------- ####
+out <- readModels("Analysis/Auxiliary_Models/mi_model2.out")
+
+out_pair <- out$parameters$unstandardized
+out_pair <- subset(out_pair, paramHeader == "New.Additional.Parameters")
+
+
+####  ------------------------ (3) Adjust P-values ------------------------ ####
+out_pair$pval_adj <- p.adjust(out_pair$pval, method = "holm")
+out_pair[, c("param", "est", "pval", "pval_adj")]
